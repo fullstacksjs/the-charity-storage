215f8642895a84a1f8870299361666ff12ce4af9
ASafaierad <frontendmonster@gmail.com>
2024-02-23 18:36:27 +0100

    add test post

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import { createApp, createRouter, eventHandler, handleCors } from 'h3';
+import {
+  createApp,
+  createRouter,
+  eventHandler,
+  handleCors,
+  readBody,
+} from 'h3';
 
 import { deleteFileHandler } from './handlers/deleteFileHandler';
 import { getFileHandler } from './handlers/getFileHandler';
 import { uploadFileHandler } from './handlers/uploadFileHandler';
 
 export const app = createApp();
+
 const router = createRouter()
   .get(
     '/',
@@ -12,7 +19,15 @@ const router = createRouter()
   )
   .get('/:id', eventHandler(getFileHandler))
   .post('/', eventHandler(uploadFileHandler))
+  .post(
+    '/test',
+    eventHandler(async (event) => {
+      console.log(await readBody(event));
+      return { body: 'Done' };
+    }),
+  )
   .delete('/:id', eventHandler(deleteFileHandler));
+
 app.use(
   eventHandler((event) => {
     handleCors(event, {
@@ -22,4 +37,5 @@ app.use(
     });
   }),
 );
+
 app.use(router);
